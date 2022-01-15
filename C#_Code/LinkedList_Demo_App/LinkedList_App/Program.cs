81c6b42771bec02625df7a7f18c1cc503e64a67a
Hommy Rosado <hommy.rosado@gmail.com>
2022-01-14 20:48:21 -0500

    Updated C_Sharp_Code / LinkedList_Demo_App

---
+++ C#_Code/LinkedList_Demo_App/LinkedList_App/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_App
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // + ----- + ----- +
+            // |   3   |  null |
+            // + ----- + ----- +
+            Node first = new Node
+            {
+                Value = 3
+            };
+            // + ----- + ----- +  + ----- + ----- +
+            // |	3  |  null +  |   5  |  null  |
+            // + ----- + ----- +  + ----- + ----- +
+            Node middle = new Node
+            {
+                Value = 5
+            };
+            // + ----- + ----- +  + ----- + ----- +
+            // |	3  |  -----+->|   5  |  null  |
+            // + ----- + ----- +  + ----- + ----- +
+            first.Next = middle;
+
+            // + ----- + ----- +  + ----- + ----- +  + ----- + ----- +
+            // |	3  |  -----+->|   5  |  null  +  |   ?   |  null |
+            // + ----- + ----- +  + ----- + ----- +  + ----- + ----- + 
+            Node last = new Node
+            {
+                Value = 7
+            };
+
+            // + ----- + ----- +  + ----- + ----- +  + ----- + ----- +
+            // |	3  |  -----+->|   5  |   ----+->||   7   |  null |
+            // + ----- + ----- +  + ----- + ----- +  + ----- + ----- + 
+            middle.Next = last;
+
+            // Iterate over each node and print the value
+            PrintList(first);
+        }
+
+        private static void PrintList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
